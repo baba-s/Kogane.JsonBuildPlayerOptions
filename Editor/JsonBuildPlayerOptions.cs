6db90b8ec658e9030d8b798f2f5ad5f5b1dd0808
baba-s <shotababa1206@gmail.com>
2023-01-11 20:07:15 +0900

    add

---
+++ Editor/JsonBuildPlayerOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane
+{
+    [Serializable]
+    [SuppressMessage( "ReSharper", "InconsistentNaming" )]
+    [SuppressMessage( "ReSharper", "NotAccessedField.Local" )]
+    public struct JsonBuildPlayerOptions
+    {
+        [SerializeField] private string[] scenes;
+        [SerializeField] private string   locationPathName;
+        [SerializeField] private string   assetBundleManifestPath;
+        [SerializeField] private string   targetGroup;
+        [SerializeField] private string   target;
+        [SerializeField] private string   subtarget;
+        [SerializeField] private string   options;
+        [SerializeField] private string[] extraScriptingDefines;
+
+        public JsonBuildPlayerOptions( BuildPlayerOptions value )
+        {
+            scenes                  = value.scenes;
+            locationPathName        = value.locationPathName;
+            assetBundleManifestPath = value.assetBundleManifestPath;
+            targetGroup             = value.targetGroup.ToString();
+            target                  = value.target.ToString();
+            subtarget               = value.subtarget.ToString();
+            options                 = value.options.ToString();
+            extraScriptingDefines   = value.extraScriptingDefines;
+        }
+
+        public override string ToString()
+        {
+            return JsonUtility.ToJson( this, true );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.JsonBuildPlayerOptions
+# Kogane Json Build Player Options
+
+BuildPlayerOptions を JSON 形式の文字列に変換できるようにする構造体
